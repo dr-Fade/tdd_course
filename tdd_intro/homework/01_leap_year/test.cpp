daceee4751c8d20a9e42c1b6b7b0fd3b32de6ab6
koshel.eugene <koshel.eugene@apriorit.com>
2019-05-23 10:33:29 +0300

    RED: function does not exist

--- tdd_intro/homework/01_leap_year/test.cpp
+++ tdd_intro/homework/01_leap_year/test.cpp
@@ -13,3 +13,7 @@ If your language provides a method in the standard library that does this look-u
 */
 
 #include <gtest/gtest.h>
+
+TEST(LeapYear, is4aLeapYear) {
+    ASSERT_EQ(true, isLeapYear(4));
+}
