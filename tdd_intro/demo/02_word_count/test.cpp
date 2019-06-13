c0972424b414a699c70dceb37100ff5e7193e165
koshel.eugene <koshel.eugene@apriorit.com>
2019-06-13 11:26:26 +0300

    Architecture + test plan

--- tdd_intro/demo/02_word_count/test.cpp
+++ tdd_intro/demo/02_word_count/test.cpp
@@ -13,6 +13,26 @@ manner: 1
 such: 1
 */
 
+/*
+ * There are going to be 2 functions:
+ * 1. the one that extracts a word from a sentence and puts them into vector;
+ * 2. the one that checks how many times unique words occur in the sentence.
+ */
+
+/*
+ * Test plan:
+ * 1. split "" -> empty vector
+ * 2. split "hello" -> vector with one element
+ * 3. split "hello world" -> vector with two elements
+ * 4. split "hello, world" -> vector with two elements
+ * 5. split "hello, world." -> vector with two elements
+ * 6. count "" -> empty map
+ * 7. count "hello world" -> {{"hello", 1}, {"world", 1}}
+ * 8. count "hello, world" -> {{"hello", 1}, {"world", 1}}
+ * 9. count "hello, world." -> {{"hello", 1}, {"world", 1}}
+ * 10. count "hello world. hello world again" -> {{"hello", 2}, {"world", 2}, {"again", 1}}
+ */
+
 #include <gtest/gtest.h>
 #include <string>
 #include <map>
